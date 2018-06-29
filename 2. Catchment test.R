cbde8c2c02d2c9f3e6594dd668b5a2883323026d
abuz5257 <alexander.buzacott@sydney.edu.au>
2018-06-29 11:10:31 +1000

    Add script 2 as R script

---
+++ 2. Catchment test.R
@@ -0,0 +1,44 @@
+#------------------------------------------------#
+# Script 2                                       #
+# Extract river accuracy data                    #
+#------------------------------------------------#
+
+#Libraries
+library(tidyverse)
+
+# Get list of river data files downloaded from BoM
+rivers <- list.files("Data/Rivers", full.names = TRUE)
+# Set up column names for use in loop
+name <- c('Date', 'Flow', 'QCode')
+# Empty dataframe to store the data in 
+river.accuracy <- data.frame(AWRC.Station.Number = rep(NA, length(rivers)), 
+                             Station.Name = rep(NA, length(rivers)),
+                             Flow.Accuracy = rep(NA, length(rivers))) 
+
+# Extract river accuracy
+for (i in 1:length(rivers)) {
+  # Read header file from data to extract AWRC river number and name
+  header <- read_csv(rivers[i], col_names = F, n_max = 26)
+  # Extract number using gsub
+  river.accuracy[i,1] <- gsub(".*\\((.*)\\).*", "\\1", header[15,2])
+  # Extract name
+  river.accuracy[i,2] <- gsub("\\s*\\([^\\)]+\\)", "", header[15,2])
+  
+  # Read river file. Skipping header and default column names
+  riverFile <- read_csv(rivers[i], skip = 27, col_names = name)
+  # Convert data to POSIXct format and extract study dates
+  riverFile$Date <- as.POSIXct(as.character(riverFile$Date), format = "%Y-%m-%d")
+  riverFile <- subset(riverFile, Date >= "1990-01-01" & Date <= "2010-12-31")
+  
+  # Find how much of the data has an accuracy code of A
+  river.accuracy[i,3] <- length(riverFile$QCode[which(riverFile$QCode == "A")])/length(riverFile$QCode)*100
+}
+# End loop
+
+# Order by accuracy and then cut off less than 90. The 90 cut off is likely redundant
+# as the data we download is already based off this cut off
+river.accuracy <- river.accuracy[order(-river.accuracy[,3], river.accuracy[,2], river.accuracy[,1]),]
+river.accuracy <- river.accuracy[which(river.accuracy$flow_accuracy > 90),] ###111
+
+# Write CSV
+write_csv(river.accuracy, path = "Data/CatchmentQuality.csv")
